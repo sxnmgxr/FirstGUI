6ee7431bed96aa3f228139ccd6d1bfdd1cf2c54f
sxnmgxr <sujanmagar05825@gmail.com>
2024-03-26 07:34:04 +0545

    "update third"

--- src/LoginView.java
+++ src/LoginView.java
@@ -1,6 +1,21 @@
-import javax.swing.JFrame;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JTextField;
+import javax.swing.*;
 
 public class LoginView extends JFrame {
+
+     JLabel jLabelUname = new JLabel("username:");
+    JTextField jTxtUname = new JTextField(8);
+    
+    JLabel jLabelPswd = new JLabel("password:");
+    JTextField jTxtPswd = new JTextField(8);
+    
+    
+JButton LoginButton = new JButton("Login");
+JButton RegisterButton = new JButton("Register");
 
     LoginView(){
         setTitle("Login into Your Account");
@@ -8,6 +23,7 @@ public class LoginView extends JFrame {
         setLocationRelativeTo(null);
         setResizable(true);
         setVisible(true);
+        userGUI();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
 
@@ -15,7 +31,26 @@ public class LoginView extends JFrame {
 
     public static void main(String[] args) {
 
+        // JFrame frame = new JFrame();
+        // JTextField textField = new JTextField();
+        // frame.add(textField);
+        // frame.setSize(300, 200);
+        // textField.setBounds(50, 50, 150, 30);
+        // frame.setVisible(true);
     new LoginView();
     }
+
+void userGUI()
+{
+    Container window = getContentPane();
+    window.setLayout(new GridLayout(10, 10));
+    window.add(jLabelUname);
+window.add(jTxtUname);
+window.add(jLabelPswd);
+window.add(jTxtPswd);
+window.add(LoginButton);
+window.add(RegisterButton);
+}
+
 
 }
