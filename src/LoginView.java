d62c635ad70b4ed0debb028882db1200ff1a7075
sxnmgxr <sujanmagar05825@gmail.com>
2024-03-26 06:46:07 +0545

    "second update"

--- src/LoginView.java
+++ src/LoginView.java
@@ -2,16 +2,20 @@ import javax.swing.JFrame;
 
 public class LoginView extends JFrame {
 
+    LoginView(){
+        setTitle("Login into Your Account");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setResizable(true);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+
+    }
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
 
-    frame.setTitle("Login");
-    frame.setSize(500, 500);
-    frame.setLocationRelativeTo(null);
-    frame.setResizable(true);
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+    new LoginView();
     }
 
 }
