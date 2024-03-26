acd6963b6da5655e378eb375b2e426bedbf0a96b
sxnmgxr <sujanmagar05825@gmail.com>
2024-03-26 06:40:16 +0545

    "java gui"

---
+++ src/LoginView.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class LoginView extends JFrame {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+    frame.setTitle("Login");
+    frame.setSize(500, 500);
+    frame.setLocationRelativeTo(null);
+    frame.setResizable(true);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
